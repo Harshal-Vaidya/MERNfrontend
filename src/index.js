1c2624aab5ad585bad767358513733cbc8023d66
harshalvaidya18@gmail.com <harshalvaidya@gmail.com>
2023-04-26 21:01:35 +0530

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+
+// import {React ,StrictMode} from 'react';
+import React from "react";
+import Routes from "./Routes";
+import ReactDOM from "react-dom";
+
+import {createRoot} from 'react-dom/client';
+//import App from './App';
+
+
+
+ReactDOM.render(<Routes/>, document.getElementById("root"));
+
+
+
+// const rootElement = document.getElementById('root');
+// const root = createRoot(rootElement);
+
+// root.render(
+//     <StrictMode>
+//       <Routes/>
+//     </StrictMode>,
+// );
